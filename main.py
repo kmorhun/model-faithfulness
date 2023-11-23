22a5b82abf409cac2f813c4225f2c73796f3faa8
kmorhun <kmorhun@gmail.com>
2023-11-23 09:30:37 -0500

    support for gpt json output

--- main.py
+++ main.py
@@ -1,37 +1,69 @@
 import os
 import openai
 from flan_call import get_hugg_completion
-from gpt_api import test
+from gpt_api import get_gpt_completion, gpt_save_output_json
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import google.generativeai as palm
 from dotenv import load_dotenv
 from prompts import *
+from datetime import datetime
 
-def run_gpt():
+prompt_1 = create_prompt(PROMPT_TEMPLATE_FEW_SHOT, FORMATTED_EXAMPLE_TEMPLATE_1, NEW_EXAMPLE_JSON_TEMPLATE, ["example_1", "example_2", "example_3"], PANCAKES_EXAMPLE)
+prompt_2 = create_prompt(PROMPT_TEMPLATE_FEW_SHOT, FORMATTED_EXAMPLE_TEMPLATE_1, NEW_EXAMPLE_JSON_TEMPLATE, ["example_1", "example_2"], PANCAKES_EXAMPLE)
+prompt_3 = create_prompt(PROMPT_TEMPLATE_ZERO_SHOT, None, NEW_EXAMPLE_JSON_TEMPLATE, [], PANCAKES_EXAMPLE, zero_shot=True)
+
+def extract_review(prompt):
+    #this gets the part of the prompt after "Human: "
+    review_assistant = prompt.split("Human: ")[-1]
+    # print("review_assistant ", review_assistant, "\n")
+
+    #this gets the actual review
+    review = review_assistant.split("\n")[0]
+    # print("review ", review, "\n")
+
+    return review
+
+def run_gpt(prompts):
+    """
+    Prompts GPT-4, and reformats the model's response into a json file, which gets saved to data/outputs
+    As a checkpoint, it also saves the intermediate step from model response to a file in data/outputs
+    """
     # print(os.environ)
     # print(os.getenv("OPENAI_API_KEY"))
     os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
     openai_api_key = os.getenv("OPENAI_API_KEY")
     openai.api_key = openai_api_key
     # get_gpt_completion("What is your name?")
-    test()
+    responses_json = {"responses": []}
+    new_reviews = []
+    for prompt in prompts:
+        new_reviews.append(extract_review(prompt))
+        response = get_gpt_completion(prompt)
+        print(response)
+        responses_json["responses"].append(response)
+
+    #save raw outputs to a file in data/outputs as a checkpoint
+    now = datetime.now()
+    now = now.strftime("%Y_%m_%d-%H_%M_%S")
+    with open(f"data/outputs/gpt_raw_output_{now}.json", "w") as raw_output_file:
+        json.dump(responses_json, raw_output_file, indent=4)
+
+    gpt_save_output_json(new_reviews, responses_json["responses"])
 
 
 def run_hugg_flan():
     tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base", token = os.getenv("HUGGING_FACE_KEY"))
     model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base", token = os.getenv("HUGGING_FACE_KEY")) 
-    prompt = create_prompt(PROMPT_TEMPLATE_FEW_SHOT, FORMATTED_EXAMPLE_TEMPLATE_1, NEW_EXAMPLE_JSON_TEMPLATE, ["example_1", "example_2"], PANCAKES_EXAMPLE)
-    get_hugg_completion(prompt, tokenizer, model)
+    get_hugg_completion(prompt_2, tokenizer, model)
     get_hugg_completion("can you tell me what sport the following prompt relates too and give me your resoning? prompt: I like to shoot balls into a circle. Answer either: basketball, baseball", tokenizer, model)
     
 
 def run_bard():
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/zackduitz/Downloads/first-website-374821-7893c1f2d758.json'
     palm.configure(api_key = os.getenv("PALM_KEY"))
-    prompt = create_prompt(PROMPT_TEMPLATE_FEW_SHOT, FORMATTED_EXAMPLE_TEMPLATE_1, NEW_EXAMPLE_JSON_TEMPLATE, ["example_1", "example_2"], PANCAKES_EXAMPLE)
-    response = palm.generate_text(prompt=prompt, model ='models/text-bison-001', temperature = 0.0, max_output_tokens = 1024)
+    response = palm.generate_text(prompt=prompt_2, model ='models/text-bison-001', temperature = 0.0, max_output_tokens = 1024)
     print( "From text-bison: ", response)
-    response2 = palm.chat(messages=prompt, model ='models/chat-bison-001', temperature = 0.0)
+    response2 = palm.chat(messages=prompt_2, model ='models/chat-bison-001', temperature = 0.0)
     print("From chat-bison", response2.last)
     # for m in palm.list_models():
     #     print(m)
@@ -45,8 +77,9 @@ if __name__ == '__main__':
     load_dotenv(dotenv_path)
 
     # Run the different model tests
-    # run_gpt()
+    prompts = [prompt_1, prompt_2]
+    run_gpt(prompts)
     # run_bard()
-    run_hugg_flan()
+    # run_hugg_flan()
 
     
--- prompts.py
+++ prompts.py
@@ -16,7 +16,7 @@ NEW_EXAMPLE_TEMPLATE = Template("Human: $review\nAssistant:")
 NEW_EXAMPLE_JSON_TEMPLATE = Template("Human: $review\n(In JSON Format) Assistant:")
 PANCAKES_EXAMPLE = "Excellent breakfast. Regular pancakes rocked. Staff was super and we were seated immediately on a Saturday. So lucky. My first 5 star rating on Yelp."
 
-PROMPT_TEMPLATE_FEW_SHOT = Template(f"{PREFIX_1}$examples\n\n$prompt")
+PROMPT_TEMPLATE_FEW_SHOT = Template(f"{PREFIX_1}\n\n$examples\n\n$prompt")
 PROMPT_TEMPLATE_ZERO_SHOT = Template(f"{PREFIX_1}\n$prompt")
 
 def create_prompt(prompt_template, formatted_example_template, new_example_template, example_names, new_example, zero_shot=False):
@@ -29,15 +29,16 @@ def create_prompt(prompt_template, formatted_example_template, new_example_templ
         new_example_template (Template): the Template object describing how to format a new example
         example_names (list[str]): a list of example names which correspond to examples.json
         new_example (str): the string body of a new review
-        zero_shot (bool): whether or not this is a zero-shot prompt. By default False. If this is true, only the prompt_template and new_example variables matter
+        zero_shot (bool): whether or not this is a zero-shot prompt. By default False. If this is true, only the prompt_template, new_example, and formatted_example_template variables matter
 
     Returns: A formatted string representing the prompt to input into a model
     """
     if (zero_shot):
-        return prompt_template.substitute({"prompt": new_example})
+        new_review = new_example_template.substitute({"review": new_example})
+        return prompt_template.substitute({"prompt": new_review})
 
     example_strings = []
-    with open("examples.json") as examples_file:
+    with open("data/inputs/examples.json") as examples_file:
         examples = json.load(examples_file)
 
         for name in example_names:
@@ -69,11 +70,11 @@ if __name__ == '__main__':
     #If you get UnicodeDecodeErrors, double check the text in your json file, especially quotation marks. 
     # Make sure these are ascii quotation marks
     
-    with open("examples.json", "r", encoding="UTF-8") as examples_file:
+    with open("data/inputs/examples.json", "r", encoding="UTF-8") as examples_file:
         few_shot_examples = ["example_1", "example_2"]
         examples = json.load(examples_file)
         test_review = PANCAKES_EXAMPLE
-        print(create_prompt(PROMPT_TEMPLATE_FEW_SHOT, FORMATTED_EXAMPLE_TEMPLATE_1, NEW_EXAMPLE_JSON_TEMPLATE, few_shot_examples, test_review))
+        print(create_prompt(PROMPT_TEMPLATE_FEW_SHOT, FORMATTED_EXAMPLE_TEMPLATE_1, NEW_EXAMPLE_JSON_TEMPLATE, few_shot_examples, test_review, zero_shot=False))
 
 # prompt = """Please classify the sentiment of the following Yelp customer reviews of restaurants with a score from 1 (negative) to 5 (positive). Explain the reasoning for your classification by highlighting the most relevant phrases in the review used in analysis.
 
